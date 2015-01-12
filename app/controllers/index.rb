46000406afde2a27a223be8b5f610a8b2be99475
anthony-ng <thomsonng123@gmail.com>
2015-01-11 17:36:00 -0800

    updated

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,13 +3,6 @@ get '/' do
 end
 
 get '/movie' do
-  # api = OMDb::Client.new
-
-  # APPLICATION_NAME = 'Httparty'
-  # all_users = HTTParty.get('http://www.omdbapi.com/?')
-  # p all_users
-
-  # p params[:movie]
 
   @movie_api = Omdb::Api.new.fetch(params[:movie])
 
@@ -23,53 +16,4 @@ get '/movie' do
   p @movie.poster
 
   erb :movie
-end
-
-# View all notes
-get '/notes' do
-  @notes = Note.all
-  erb :index
-end
-
-# Return a submit form for creating a new note
-get '/notes/new' do
-  erb :new
-end
-
-# Create a new note
-post '/notes' do
-  Note.create(title: params[:title], content: params[:content])
-  redirect '/notes'
-end
-
-# Display a specific note
-get '/notes/:id' do
-  @note = Note.find(params[:id])
-  erb :display
-end
-
-# Return a submit form for updating a note
-get '/notes/:id/edit' do
-  @note = Note.find(params[:id])
-  erb :update
-end
-
-# Update a specific note
-put '/notes/:id' do
-  @note = Note.find(params[:id])
-  @note.update(title: params[:title], content: params[:content] )
-  redirect '/notes'
-
-end
-
-get '/notes/:id/delete' do
-  @note = Note.find(params[:id])
-  erb :delete
-end
-
-# Delete a specific note
-delete '/notes/:id' do
-  @note = Note.find(params[:id])
-  @note.destroy
-  redirect '/notes'
 end
