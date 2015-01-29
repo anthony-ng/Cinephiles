7c688a3fb469f6ae08bf899fa5796224f84d5089
anthony-ng <thomsonng123@gmail.com>
2015-01-28 23:20:54 -0800

    fixed comingsoon link

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,12 @@
 $('document').ready( function(){
 
+  // Footer Date
   var today = new Date();
   var date = today.toDateString();
   var footer_el = document.getElementById('footer');
   footer_el.innerHTML = '<h4>Copyright &copy; ' + date + '</h4><h4>Anthony Ng | Crafted lovingly in San Francisco, California.</h4>';
-  // document.write(year)
+
+
 
 
 
