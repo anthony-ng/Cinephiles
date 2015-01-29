27b24e1f56c81d4e3e2dc4cd0ef3c951d236cc3f
anthony-ng <thomsonng123@gmail.com>
2015-01-28 23:12:43 -0800

    added JS for footer date

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,22 @@
-// $('document').ready( function(){
-//   $('form').on('submit', function(event){
-//     event.preventDefault()
-//     $('form').nextAll().remove()
-//     $.ajax({
-//       type: 'GET',
-//       url: $(this).attr('action'),
-//       data: $(this).serialize()
-//     }).done(function(data) {
-//       $(".container").append(data)
-//     })
-//   })
-// })
+$('document').ready( function(){
+
+  var today = new Date();
+  var date = today.toDateString();
+  var footer_el = document.getElementById('footer');
+  footer_el.innerHTML = '<h4>Copyright &copy; ' + date + '</h4><h4>Anthony Ng | Crafted lovingly in San Francisco, California.</h4>';
+  // document.write(year)
+
+
+
+  // $('form').on('submit', function(event){
+  //   event.preventDefault()
+  //   $('form').nextAll().remove()
+  //   $.ajax({
+  //     type: 'GET',
+  //     url: $(this).attr('action'),
+  //     data: $(this).serialize()
+  //   }).done(function(data) {
+  //     $(".container").append(data)
+  //   })
+  // })
+})
