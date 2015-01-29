e8a3d0bc8b49c8f0b3c44dfd7a48834d1f2b5f7a
anthony-ng <thomsonng123@gmail.com>
2015-01-29 01:27:02 -0800

    updated google map with search places

--- public/js/application.js
+++ public/js/application.js
@@ -8,19 +8,30 @@ $('document').ready( function(){
 
 //Google Map API
 var map;
+var infowindow;
 
 function initialize() {
-  var mapOptions = {
-    zoom: 6
+  var pyrmont = new google.maps.LatLng(-33.8665433, 151.1956316);
+
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: pyrmont,
+    zoom: 15
+  });
+
+  var request = {
+    location: pyrmont,
+    radius: 500,
+    types: ['store']
   };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
 
   // Try HTML5 geolocation
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var pos = new google.maps.LatLng(position.coords.latitude,
-                                       position.coords.longitude);
+       position.coords.longitude);
 
       var infowindow = new google.maps.InfoWindow({
         map: map,
@@ -55,7 +66,29 @@ function handleNoGeolocation(errorFlag) {
   map.setCenter(options.position);
 }
 
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
 google.maps.event.addDomListener(window, 'load', initialize);
+
 
 
 
