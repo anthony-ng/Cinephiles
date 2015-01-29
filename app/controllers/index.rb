332798d0e90e05c3bfd00aff8e93f3e8372d9ccf
anthony-ng <thomsonng123@gmail.com>
2015-01-28 22:33:48 -0800

    updated popular routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -44,17 +44,29 @@ get '/about' do
 end
 
 get '/popular' do
-  popular_movie1 = Omdb::Api.new.fetch('The Hunger Games')
+  popular_movie1 = Omdb::Api.new.fetch('AMERICAN SNIPER')
   @popular_movie1 = popular_movie1[:movie]
 
-  popular_movie2 = Omdb::Api.new.fetch('The Dark Knight')
+  popular_movie2 = Omdb::Api.new.fetch('THE BOY NEXT DOOR')
   @popular_movie2 = popular_movie2[:movie]
 
-  popular_movie3 = Omdb::Api.new.fetch('The Avengers')
+  popular_movie3 = Omdb::Api.new.fetch('PADDINGTON')
   @popular_movie3 = popular_movie3[:movie]
 
-  popular_movie4 = Omdb::Api.new.fetch('The Interview')
+  popular_movie4 = Omdb::Api.new.fetch('THE HOBBIT: THE BATTLE OF THE FIVE ARMIES')
   @popular_movie4 = popular_movie4[:movie]
+
+  popular_movie5 = Omdb::Api.new.fetch('TAKEN 3')
+  @popular_movie5 = popular_movie4[:movie]
+
+  popular_movie6 = Omdb::Api.new.fetch('THE IMITATION GAME')
+  @popular_movie6 = popular_movie4[:movie]
+
+  popular_movie7 = Omdb::Api.new.fetch('STRANGE MAGIC')
+  @popular_movie7 = popular_movie4[:movie]
+
+  popular_movie8 = Omdb::Api.new.fetch('SELMA')
+  @popular_movie8 = popular_movie4[:movie]
 
   erb :popular
 end
