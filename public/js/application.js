6409c0efd6ce712aee2ce13261bc03d6d125f1b2
anthony-ng <thomsonng123@gmail.com>
2015-01-29 00:56:23 -0800

    added google map

--- public/js/application.js
+++ public/js/application.js
@@ -6,6 +6,17 @@ $('document').ready( function(){
   var footer_el = document.getElementById('footer');
   footer_el.innerHTML = '<h4>Copyright &copy; ' + date + '</h4><h4>Anthony Ng | Crafted lovingly in San Francisco, California.</h4>';
 
+  var map;
+  function initialize() {
+    var mapOptions = {
+      zoom: 8,
+      center: new google.maps.LatLng(-34.397, 150.644)
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
